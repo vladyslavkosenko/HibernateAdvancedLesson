91a2f0b06e19092d6f1b51212e87bf0abbddec6d
vladyslavkosenko <vaduha91@gmail.com>
2022-09-02 12:04:58 +0300

    first commit

---
+++ src/main/java/com/example/HibernateAdvancedLesson/HibernateAdvancedLessonApplication.java
@@ -0,0 +1,13 @@
+package com.example.HibernateAdvancedLesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateAdvancedLessonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateAdvancedLessonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HibernateAdvancedLesson/HibernateAdvancedLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HibernateAdvancedLesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateAdvancedLessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
